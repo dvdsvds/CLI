308efb92612b2288b938993eeb022fbe3b77318e
dvdsvds <kundr5@naver.com>
2024-04-18 14:31:08 +0900

    First commit

--- CLI/command.h
+++ CLI/command.h
@@ -1,1 +1,8 @@
-#pragma once
+#ifdef COMMAND_H_
+#define COMMAND_H_
+
+class Command {
+public:
+
+};
+#endif // COMMAND_H_
---
+++ CLI/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "command.h"
+
+using namespace std;
+
+int main() {
+	return 0;
+}
